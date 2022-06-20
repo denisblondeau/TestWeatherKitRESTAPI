5dc282bc409911be37c7b93a46ea0cf60d096d69
Denis Blondeau <dbappdev@outlook.com>
2022-06-20 11:32:24 -0400

    Initial Commit

---
+++ TestWeatherKitRESTAPI/TestWeatherKitRESTAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestWeatherKitRESTAPIApp.swift
+//  TestWeatherKitRESTAPI
+//
+//  Created by Denis Blondeau on 2022-06-20.
+//
+
+import SwiftUI
+
+@main
+struct TestWeatherKitRESTAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
